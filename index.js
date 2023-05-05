a10b561328fa33a92122c235e39de11d3ea58fdc
Vorona Alexey <vav840@gmail.com>
2023-05-05 13:16:22 +0300

    My homework

---
+++ index.js
@@ -0,0 +1,27 @@
+let num1 = 8;
+let num2 = 4;
+console.log(num1*num2);
+console.log(num1/num2);
+console.log(num1**num2);
+
+let notBull = 12;
+console.log(!!notBull);
+
+let notInt = '124';
+console.log(+notInt);
+
+let notStr = 234;
+let temp = notStr + '';
+console.log(typeof (temp));
+console.log(temp);
+
+let num = 1;
+num +=5;
+console.log(num);
+num -=2;
+console.log(num);
+num **=3;
+console.log(num);
+num /=4;
+console.log(num);
+
